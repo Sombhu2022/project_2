6d8977a8582d690725067356d757a30a2090b28d
Pritam <123.paulpritam@gmail.com>
2024-04-28 20:36:24 +0530

    class

--- src/pages/layout/header/Navbar.jsx
+++ src/pages/layout/header/Navbar.jsx
@@ -1,27 +1,36 @@
-import { Link, NavLink} from 'react-router-dom'
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../media/logo.png";
 
 import { AiFillHome } from "react-icons/ai";
+import { MdInfo } from "react-icons/md";
+import { IoBagHandle } from "react-icons/io5";
+import { BiLogInCircle } from "react-icons/bi";
 
-import './navbar.scss'
+import "./navbar.scss";
 const Navbar = () => {
-  return (
-    <header className='navbar-container'>
-       <img/>
+    return (
+        <header className="navbar-container">
+            <img src={logo} className="logo" />
 
+            <nav className="navLink_container">
+                <NavLink className="link" to="/">
+                    {" "}
+                    <AiFillHome /> Home
+                </NavLink>
+                <NavLink className="link" to="/about">
+                    <MdInfo /> About
+                </NavLink>
+                <NavLink className="link" to="/product">
+                    <IoBagHandle /> product
+                </NavLink>
 
-        <nav>
-        <NavLink className="link" to="/"> <AiFillHome/> Home</NavLink>
-        <NavLink className="link" to="/about">About</NavLink>
-        <NavLink className="link" to="/product">product</NavLink>
+                <Link to={"/login"} className="loginBtn">
+                    <BiLogInCircle />
+                    Login
+                </Link>
+            </nav>
+        </header>
+    );
+};
 
-        <Link to={"/login"}>
-           <button >Login</button>
-        </Link>
-        
-        </nav>
-       
-    </header>
-  )
-}
-
-export default Navbar
+export default Navbar;
