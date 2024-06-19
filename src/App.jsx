3738483c4692536017c601319b44045df9b9c360
Sombhu2022 <sombhudas93@gmail.com>
2024-06-19 07:41:22 +0530

    19jun

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,6 @@ const App = () => {
 
 export default App;
 
-// export const baseUrl = 'http://localhost:8080'
+export const baseUrl = 'http://localhost:8080'
 
-export const baseUrl = 'https://local-bazar-server-pied.vercel.app'
+// export const baseUrl = 'https://local-bazar-server-pied.vercel.app'
